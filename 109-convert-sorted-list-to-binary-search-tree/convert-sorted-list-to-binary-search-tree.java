8343e68c8b6938eb54fe6bd637113f98a28e2be1
Kartik247-coder <kartikbobde234@gmail.com>
2026-09-07 19:12:52 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (58.80%) - LeetSync

---
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head==null) return null;
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        
+        TreeNode root=new TreeNode(slow.val);
+
+        if(slow==fast) return root;
+
+        ListNode temp=head;
+
+        while(temp.next!=slow){
+            temp=temp.next;
+        }
+    
+        temp.next=null;
+
+      
+
+        root.left=sortedListToBST(head);
+        root.right=sortedListToBST(slow.next);
+        return root;
+    }
+}
